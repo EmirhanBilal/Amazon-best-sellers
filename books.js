9b4010466968ee874760449a66448c9227e6ced3
EmirhanBilal <115393998+EmirhanBilal@users.noreply.github.com>
2023-01-28 10:20:59 +0300

    Add files via upload

---
+++ books.js
@@ -0,0 +1,17 @@
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+
+export const books = [
+  {
+    id: 1,
+    author: "Prince Harry",
+    title: "Spare Hardcover",
+    img: img1,
+  },
+  {
+    id: 2,
+    author: "Rick Rubin",
+    title: "The Creative Act: A Way of Being",
+    img: img2,
+  },
+];
